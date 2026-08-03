b0767eec813813c5010507d37c92753da413ea60
siavash <you@example.com>
2026-08-03 21:32:13 +0330

    Add environment variable support and refactor NVIDIA API key usage in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from src.data_loading import load_data, URLS
 from src.vector_store.indexing import MultiRepresentation, RAPTOR,RAPTORRetrieverQdrant
 from langchain_ollama import ChatOllama
@@ -18,13 +19,14 @@ load_dotenv(".env")
 vector_client = QdrantClient()
 
 llm= ChatOllama(model="qwen3.5:4b")
-embedding_model = NVIDIAEmbeddings(model="nvidia/nv-embedcode-7b-v1", api_key="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")
+embedding_model = NVIDIAEmbeddings(model="nvidia/nv-embedcode-7b-v1", api_key=os.getenv("NVIDIA_API_KEY"))
 mongo_db_store = MongoDBDocStore.from_connection_string(connection_string="mongodb://admin:admin@localhost:27017/",namespace="RAG.docstore")
 # documents = load_data()
 vector_store = QdrantVectorStore(client=vector_client, collection_name="test_collection", embedding=embedding_model)
 raptor = RAPTOR(llm=llm, vector_store=vector_store, byte_store=mongo_db_store)
 
+#raptor.index()
 
-template = "please answer **ONLY** base on this contexts:\n\n {context}\n\n question is: {question}"
+template = "Please answer **ONLY** base on this contexts:\n\n {context}\n\n question is: {question}"
 chain = StepBack(llm=llm, retriever=raptor.retriever)
 print(chain.invoke("what is the backend in Deep Agent and what is for"))
